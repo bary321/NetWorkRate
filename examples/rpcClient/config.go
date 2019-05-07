3f92ac80e160d0205b478c3e5dc93120698f7606
bary321 <wyj13688@gmail.com>
2019-05-07 14:43:21 +0800

    更新Config结构

--- examples/rpcClient/config.go
+++ examples/rpcClient/config.go
@@ -1,15 +1,52 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/bary321/NetWorkRate"
+	"io/ioutil"
+	"os"
+)
+
 type Config struct {
 	Servers []*Server     `json:"servers"`
 	Default *DefaultInter `json:"default"`
 }
 
 type Server struct {
-	Ip string `json:"ip"`
+	Ip    string               `json:"ip"`
+	Wan   string               `json:"wan,omitempty"`
+	Lan   string               `json:"wan,omitempty"`
+	Rates *NetWorkRate.IORates `json:"rates, omitempty"` //理论上这个不应该出现在配置文件中
 }
 
 type DefaultInter struct {
 	Wan string `json:"wan"`
 	Lan string `json:"lan"`
+}
+
+func (c *Config) Get(filepath string) error {
+	f, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	d, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(d, c)
+	if err != nil {
+		return err
+	}
+	length := len(c.Servers)
+	for i := 0; i < length; i++ {
+		if c.Servers[i].Wan == "" {
+			c.Servers[i].Wan = c.Default.Wan
+		}
+		if c.Servers[i].Lan == "" {
+			c.Servers[i].Lan = c.Default.Lan
+		}
+	}
+
+	return nil
+
 }
--- examples/rpcClient/config_test.go
+++ examples/rpcClient/config_test.go
@@ -24,5 +24,17 @@ func TestStruct(t *testing.T) {
 	}
 	fmt.Printf("%s\n", b)
 	fmt.Println(c)
+}
 
+func TestGet(t *testing.T) {
+	c := new(Config)
+	err := c.Get("./example.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if c.Servers[0].Wan != "eth1" || c.Servers[0].Lan != "eth0" {
+		t.Error("get function fail")
+		t.Error(c.Servers[0])
+	}
 }
